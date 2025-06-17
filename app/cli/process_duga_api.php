8b04f22881ba498ec7f5573ee52ce3cccd367e10
bicstation <bicstation@gmail.com>
2025-06-18 08:23:02 +0900

    proceed_dduga_api取得生データ10

--- app/cli/process_duga_api.php
+++ app/cli/process_duga_api.php
@@ -19,7 +19,7 @@ use App\Core\Logger;
 use App\Core\Database;
 use App\Api\DugaApiClient;
 use App\Util\DbBatchInsert;
-use PDOException; // PDOExceptionをuseしておく
+// use PDOException; // PDOExceptionをuseしておく
 
 // このスクリプトがCLI (コマンドラインインターフェース) から実行されたことを確認
 if (php_sapi_name() !== 'cli') {
