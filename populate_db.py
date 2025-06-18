f32bb14e1a4d8bac79dd9e20fb75fea9c99a0b33
bicstation <bicstation@gmail.com>
2025-06-18 11:38:44 +0900

    revuild 取得全変更

---
+++ populate_db.py
@@ -0,0 +1,17 @@
+import os
+import mysql.connector
+import json
+from datetime import datetime
+
+DB_CONFIG = {
+    'host': os.getenv('DB_HOST', 'localhost'), # Docker Composeのサービス名 'mysql' が渡される
+    'user': os.getenv('DB_USER', 'root'),
+    'password': os.getenv('DB_PASS', 'your_db_password'),
+    'database': os.getenv('DB_NAME', 'tiper')
+}
+
+# ... その他の関数 (変更なし) ...
+
+# メイン処理の関数 (変更なし)
+def populate_products_from_raw_data():
+    # ...
